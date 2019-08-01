b66fd5f6ebd0cbdc1924ecd6cbe24dfd5c41ba99
matthenge <matthenge@gmail.com>
2019-08-01 20:50:55 +0300

    day3

---
+++ functons.py
@@ -0,0 +1,9 @@
+#function is a block of code/statements that performs a certain task and retern a resalt/value
+def addtwonumbers():
+    sum=34+56
+    return sum
+print(type(addtwonumbers()))
+def addnumber(a,b):
+   sum = a+b
+   return sum
+print(addnumber(53,456))
